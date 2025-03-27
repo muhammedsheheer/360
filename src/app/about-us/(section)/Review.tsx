67d419e20d4cf68bebe35b2d23dfaf415a5b5b16
sheheer <muhammedsheheer99@gmail.com>
2025-03-27 16:58:22 +0530

    review page edited

--- src/app/about-us/(section)/Review.tsx
+++ src/app/about-us/(section)/Review.tsx
@@ -63,9 +63,9 @@ const Reviews = ({}) => {
     // };
   }, []);
   return (
-    <section className="relative flex h-full w-full justify-center bg-[#000]">
+    <section className="relative flex h-full w-full justify-center bg-[#000] px-0 md:px-[350px]">
       <div className="flex h-full w-full flex-col items-start justify-center gap-4 py-12 md:py-44">
-        <div className="flex h-full w-full flex-col items-center justify-center gap-2 lg:gap-4">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-2 pb-2 md:pb-6 lg:gap-4">
           <h2 className="text-center font-playfair text-4xl font-[400] capitalize text-[#FBEAD2] md:text-6xl">
             Listen from our people{" "}
           </h2>
@@ -73,34 +73,38 @@ const Reviews = ({}) => {
         <div className="flex w-full items-center justify-center p-4">
           {reviews && (
             <Carousel className="w-full px-4">
-              <CarouselContent className="ml-4 flex w-full justify-center gap-4">
+              <CarouselContent className="ml-4 flex w-full justify-center gap-4 md:gap-14">
                 {reviews.map((review, index) => (
                   <CarouselItem
                     key={index}
-                    className="flex w-full basis-full flex-col gap-6 rounded-2xl border border-[#CFAC6A] bg-[#000] px-6 py-8"
+                    className="flex h-[400px] w-full max-w-[700px] basis-full flex-col items-center justify-center gap-6 rounded-none border border-[#CFAC6A] bg-[#000] px-6 py-8 md:h-[450px]"
                   >
-                    <div className="flex w-full">
+                    <div className="flex w-full items-center justify-center">
                       {Array.from({ length: review.rating }).map((_, index) => (
                         <Icons.star key={index} className="text-[#B99647]" />
                       ))}
                     </div>
-                    <div className="">
-                      <p className="text-[#fff]">{review.text}</p>
+                    <div className="flex items-center justify-center">
+                      <p className="line-clamp-5 text-center text-[#fff]">
+                        {review.text.text}
+                      </p>
                     </div>
-                    <div className="flex w-full items-center gap-2">
+                    <div className="flex w-full flex-col items-center gap-2">
                       <Image
                         src={
-                          review.profile_photo_url ||
+                          review.authorAttribution.photoUri ||
                           "/images/home/reviews/pictures/anna-mathew.svg"
                         }
                         width={64}
                         height={64}
-                        alt={review.author_name}
+                        alt={review.authorAttribution.displayName}
                       />
                       <div className="flex flex-col gap-2">
-                        <p className="text-[#CDAE64]">{review.author_name}</p>
-                        <span className="text-[#CDAE64]">
-                          {review.relative_time_description}
+                        <p className="text-center text-[#fff]">
+                          {review.authorAttribution.displayName}
+                        </p>
+                        <span className="text-center text-[#fff]">
+                          {review.relativePublishTimeDescription}
                         </span>
                       </div>
                     </div>
