4ba8b791b1dddf3ec0f350f1d161cb009837ced6
MernStack123 <muhammedsheheer99@gmail.com>
2025-05-17 13:23:46 +0530

    table booking updated

--- src/app/table-booking/(section)/Hero.tsx
+++ src/app/table-booking/(section)/Hero.tsx
@@ -2,14 +2,13 @@ const Hero = ({}) => {
   return (
     <section
       id="hero"
-      className="relative flex h-full min-h-[400px] w-full flex-col items-center justify-center gap-[1.38rem] text-[1.5rem] leading-[110%]"
+      className="flex h-full min-h-[400px] w-full flex-col items-center justify-center gap-[1.38rem] text-[1.5rem] leading-[110%]"
       style={{
         background:
           "linear-gradient(170deg, rgba(0, 0, 0, 0.00) 7.51%, rgba(0, 0, 0, 0.80) 92.93%), url(/images/about-us/6.jpg) lightgray 50% / cover no-repeat",
       }}
     >
-      <div className="absolute inset-0 bg-black/50" />
-      <h1 className="z-10 w-full text-center font-oswald text-[4.75rem] leading-[80%]">
+      <h1 className="font-oswald w-full text-center text-5xl leading-[80%] text-[#fff] md:text-[4.75rem]">
         Table Booking
       </h1>
     </section>
